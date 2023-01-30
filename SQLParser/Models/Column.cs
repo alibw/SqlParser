d37257661e47b7904b6a0af21bb8513bb81b9b9e
alibw <69758323+alibw@users.noreply.github.com>
2023-01-30 16:49:04 +0330

    Removed : removed unused properties

--- SQLParser/Models/Column.cs
+++ SQLParser/Models/Column.cs
@@ -3,8 +3,6 @@
 public class Column
 {
     public string Name { get; set; }
-    public string Value { get; set; }
     public string Type { get; set; }
     public bool Nullable { get; set; }
-    public string MaxLength { get; set; }
 }
