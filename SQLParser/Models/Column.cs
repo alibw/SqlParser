fb66f5faa039e41976dc9da15e5db10984f94609
alibw <69758323+alibw@users.noreply.github.com>
2023-01-31 12:49:43 +0330

    Added : overriding Equals to value equals

--- SQLParser/Models/Column.cs
+++ SQLParser/Models/Column.cs
@@ -1,8 +1,28 @@
 ﻿namespace SQLParser;
 
-public class Column
+public class Column :IEquatable<Column>
 {
     public string Name { get; set; }
     public string Type { get; set; }
     public bool Nullable { get; set; }
+
+    public bool Equals(Column? other)
+    {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Name == other.Name && Type == other.Type && Nullable == other.Nullable;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((Column)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Name, Type, Nullable);
+    }
 }
