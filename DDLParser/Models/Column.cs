29ae65b22a05dbfde1804f3e82dd471a83260ad2
alibw <69758323+alibw@users.noreply.github.com>
2023-04-10 12:34:54 +0330

    Added : foreignKeyTableName

--- DDLParser/Models/Column.cs
+++ DDLParser/Models/Column.cs
@@ -5,12 +5,11 @@ public class Column :IEquatable<Column>
     public string Name { get; set; }
     public string Type { get; set; }
     public bool Nullable { get; set; }
-
     public bool IsIdentity { get; set; }
-
     public bool IsPrimaryKey { get; set; }
-
     public bool IsForeignKey { get; set; }
+    
+    public string ForeignKeyTableName { get; set; }
 
     public Table? ForeignKeyTable { get; set; }
 
